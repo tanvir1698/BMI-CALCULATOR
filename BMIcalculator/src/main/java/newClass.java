c5d6f51b06ae6dc54870fcbfc2543a41ded540a0
tanvir1698 <ahmmedtanvir98@gmail.com>
2022-05-11 01:47:07 +0600

    add random file

---
+++ BMIcalculator/src/main/java/newClass.java
@@ -0,0 +1,24 @@
+public class newClass {
+    int x,y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public newClass(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
