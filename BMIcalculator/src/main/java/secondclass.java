80c2b15bab5c9f3fef9bff54488fda96660aa175
tanvir1698 <ahmmedtanvir98@gmail.com>
2022-05-11 02:27:13 +0600

    modified secondclass from b2

--- BMIcalculator/src/main/java/secondclass.java
+++ BMIcalculator/src/main/java/secondclass.java
@@ -1,4 +1,25 @@
 public class secondclass {
     int x;
-    
+    double y;
+
+    public secondclass(int x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
